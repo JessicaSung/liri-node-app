250be85c29681bf10f38b192c400cd7658fafdca
JessicaSung <jessica.sung@utexas.edu>
2016-10-12 11:57:20 -0500

    obtained twitter api keys and input informations in keys.js file. edited random.txt. unsure on liri.js accuracy.

---
+++ liri.js
@@ -0,0 +1,20 @@
+// CREATE JSON PACKAGE AND SAVE NODE MODULES: TWITTER, SPOTIFY, REQUEST, OMDB, FS
+
+
+// VARIABLES
+// Includes the request package for grabbing data from OMDB 
+var request = require('request');
+
+// Includes the FS package for reading and writing packages 
+var fs = require('fs');
+
+
+// Running the readFile module that's inside of fs.
+// Stores the read information into the variable "data"
+fs.readFile("random.txt", "utf8", function(err,data){
+}
+
+consumer_key: keys.twitterKeys.consumer_key,
+consumer_secret: keys.twitterKeys.consumer_secret,
+access_token_key: keys.twitterKeys.access_token_key,
+access_token_secret: keys.twitterKeys.access_token_secret
