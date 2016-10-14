22733842b52ed29abce1d452d0dfc2358ff644a7
JessicaSung <jessica.sung@utexas.edu>
2016-10-14 11:16:33 -0500

    Alex helped: remove fs dependency from package.json (it is built into node.) console log keys.twitterKeys as it is exported from another file. grabbing user command from node with process.argv[2] and setting it to a variable. Creating a new object with Twitter function and setting it to a variable. using variables from Twitter's npm documentation. Writting a switch command. Writing the command for the function referenced in the switch function with client.get function inside.

--- liri.js
+++ liri.js
@@ -1,20 +1,80 @@
-// CREATE JSON PACKAGE AND SAVE NODE MODULES: TWITTER, SPOTIFY, REQUEST, OMDB, FS
+// CREATE JSON PACKAGE AND SAVE NODE MODULES: TWITTER, SPOTIFY, REQUEST, OMDB
 
 
 // VARIABLES
-var keys = require('./key.js');
+var keys = require('./keys.js');
+console.log(keys.twitterKeys);
 
 // // Includes the request package for grabbing data from OMDB 
-// var request = require('request');
+var request = require('request');
 
-// // Includes the FS package for reading and writing packages 
-// var fs = require('fs');
+// Includes the FS package for reading and writing packages 
+var fs = require('fs');
+
+
+// grab user commands from node
+var command = process.argv[2];
+
+var Twitter = require('twitter'); 
+var client = new Twitter(keys.twitterKeys); 
+var params = {screen_name: 'nodejs'};
+
+
+
+
+switch (command) {
+	case "my-tweets":		
+		grabTweets();
+		break;
+	case "spotify-this-song":
+		console.log("Spotify this song for me");
+		break;
+	case "movie-this":
+		console.log("Find rotten tomatoes rating");
+		break;
+	case "do-what-it-says":
+		console.log("Do what I say!!");
+		break;
+	default:
+		console.log("Invalid command");
+}
+
+
+
+function grabTweets() {
+	console.log("Go grab my tweets");
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    console.log(tweets);
+	  }
+	});
+}
+
+
+
+
+
+
+
+
+
+
 
 
 // // Running the readFile module that's inside of fs.
 // // Stores the read information into the variable "data"
 // fs.readFile("random.txt", "utf8", function(err,data){
+// 	var params = {screen_name: 'nodejs'};
+
+// 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+// 	  if (!error) {
+// 	    console.log(tweets);
+// 	  }
+// 	});
+
 // }
+
+
 
 // consumer_key: keys.twitterKeys.consumer_key,
 // consumer_secret: keys.twitterKeys.consumer_secret,
