fbc7116c4f14b1210003bb7a426edaddd4da9a9c
JessicaSung <jessica.sung@utexas.edu>
2016-10-15 17:25:06 -0500

    Output the artist name. Required OMDB.

--- liri.js
+++ liri.js
@@ -3,19 +3,23 @@
 
 // VARIABLES
 var keys = require('./keys.js');
-// console.log(keys.twitterKeys);
-
-// // Includes the request package for grabbing data from OMDB 
 var request = require('request');
-
-// Includes the FS package for reading and writing packages 
 var fs = require('fs');
+var spotify = require('spotify');
+var omdb = require('omdb');
+
 
 // grab user commands from node
 var command = process.argv[2];
+var mediaName = "";
+
+	for (var i = 3; i < process.argv.length; i++) {
+		mediaName += "+" + process.argv[i];
+	}
 
 var Twitter = require('twitter'); 
 var client = new Twitter(keys.twitterKeys); 
+// console.log(keys.twitterKeys);
 var params = {screen_name: 'JessicaSung_'};
 
 
@@ -38,19 +42,6 @@ switch (command) {
 }
 
 
-
-function spotifyThis() {
-	// console.log("Spotify this song for me");
-}
-
-function movieThis() {
-	// console.log("Find rotten tomatoes rating");
-}
-
-function doThis() {
-	// console.log("Do what I say!!");
-}
-
 function grabTweets() {
 	// console.log("Go grab my tweets");
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -69,4 +60,34 @@ function grabTweets() {
 	    }	    
 	  }
 	});
+}
+
+
+function spotifyThis() {
+	// console.log("Spotify this song for me");
+	if (mediaName == null) {
+		mediaName = "The Sign";
+	}
+
+	spotify.search({ type: 'track', query: mediaName = "The Sign"}, function(err, data) {	    
+	    if ( err ) {
+	        console.log('Error occurred: ' + err);
+	        return;
+	    }
+		
+	    console.log(data);
+	    // console.log("Artist: " + data.tracks.items[0].artists[0].name);
+	    // console.log("Name: " + );
+	    // console.log("Link: " + );
+	    // console.log("Album: " + );
+	});
+
+}
+
+function movieThis() {
+	// console.log("Find rotten tomatoes rating");
+}
+
+function doThis() {
+	// console.log("Do what I say!!");
 }
